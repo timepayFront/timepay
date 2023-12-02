173828cb25e7d7384ca7aebf2f5cbae42602d905
211_0@naver.com <211_0@naver.com>
2023-12-03 03:04:46 +0900

    글쓰기 버튼 글자 부분 눌러야만 작동되는 문제 해결

--- src/front-end/timepay-front/src/pages/HomePage/HomePage.tsx
+++ src/front-end/timepay-front/src/pages/HomePage/HomePage.tsx
@@ -113,9 +113,14 @@ const HomePage = () => {
     return false;
   }, [userInfoData]);
 
-  const handleOnLinkWrite = () => {
-    navigate(PATH.WritePage);
+  const handleOnLinkRequest = () => {
+    navigate(PATH.Register_HR);
   };
+
+  const handleOnLinkWith = () => {
+    navigate(PATH.Register_HS);
+  };
+
 
   const handleOnShowQRModal = useCallback(() => {
     Modal.confirm({
@@ -187,8 +192,8 @@ const HomePage = () => {
       </div>
       <div css={cssWriteContainer}>
         {/* <div style={{position: 'fixed', width: '100vw', height: '79vh', display: 'flex', flexDirection: 'column'}}> */}
-          <Button css={cssBtnStyle1}><Link to={PATH.Register_HR}>도움요청<br/>도움이 필요할 때<br/>다른 분에게 요청해보세요!</Link></Button>
-          <Button css={cssBtnStyle1}><Link to={PATH.Register_HS}>같이하기<br/>마음이 맞는 사람끼리<br/>같이 활동해보세요!</Link></Button>
+          <Button onClick={handleOnLinkRequest} css={cssBtnStyle1}><Link to={PATH.Register_HR}>도움요청<br/>도움이 필요할 때<br/>다른 분에게 요청해보세요!</Link></Button>
+          <Button onClick={handleOnLinkWith} css={cssBtnStyle1}><Link to={PATH.Register_HS}>같이하기<br/>마음이 맞는 사람끼리<br/>같이 활동해보세요!</Link></Button>
           <Button onClick={handleOnShowQRModal} css={cssBtnStyle1}><Link to={PATH.Register_HR}>바로도움요청<br/>급하게 도움이 필요할 때<br/>도움을 요청해보세요!</Link></Button>
         {/* </div> */}
       </div>
--- src/front-end/timepay-front/src/pages/WritePage/WritePage.tsx
+++ src/front-end/timepay-front/src/pages/WritePage/WritePage.tsx
@@ -23,9 +23,14 @@ import {
       return false;
     }, [data]);
   
-    const handleOnLinkWrite = () => {
-      navigate(PATH.WritePage);
+    const handleOnLinkRequest = () => {
+      navigate(PATH.Register_HR);
     };
+
+    const handleOnLinkWith = () => {
+      navigate(PATH.Register_HS);
+    };
+
   
     const handleOnShowQRModal = useCallback(() => {
       Modal.confirm({
@@ -37,34 +42,6 @@ import {
         },
       });
     }, [data]);
-
-    // const items: MenuProps['items'] = useMemo(() => {
-    //   const items = [
-    //     {
-    //       key: PATH.Register_HR,
-    //       label: <Link to={PATH.Register_HR}>도움요청</Link>,
-    //     },
-    //     {
-    //       key: PATH.Register_HS,
-    //       label: <Link to={PATH.Register_HS}>같이하기</Link>,
-    //     },
-    //   ];
-    //   if (isAgency)
-    //     items.push({
-    //       key: PATH.DONATION_BOARD_WRITE,
-    //       label: <Link to={PATH.DONATION_BOARD_WRITE}>기부하기</Link>,
-    //     });
-    //   else
-    //     items.push({
-    //       key: '바로도움요청',
-    //       label: (
-    //         <Button type="link" onClick={handleOnShowQRModal}>
-    //           바로도움요청
-    //         </Button>
-    //       ),
-    //     });
-    //   return items;
-    // }, [isAgency, handleOnShowQRModal]);
   
     useEffect(() => {
       setHeaderTitle('글쓰기');
@@ -74,8 +51,8 @@ import {
       <>
       {/* <div css={cssWriteContainer}> */}
       <div style={{position: 'fixed', width: '100vw', height: '79vh', display: 'flex', flexDirection: 'column'}}>
-          <Button css={cssBtnStyle1}><Link to={PATH.Register_HR}>도움요청<br/>도움이 필요할 때<br/>다른 분에게 요청해보세요!</Link></Button>
-          <Button css={cssBtnStyle1}><Link to={PATH.Register_HS}>같이하기<br/>마음이 맞는 사람끼리<br/>같이 활동해보세요!</Link></Button>
+          <Button onClick={handleOnLinkRequest} css={cssBtnStyle1}><Link to={PATH.Register_HR}>도움요청<br/>도움이 필요할 때<br/>다른 분에게 요청해보세요!</Link></Button>
+          <Button onClick={handleOnLinkWith} css={cssBtnStyle1}><Link to={PATH.Register_HS}>같이하기<br/>마음이 맞는 사람끼리<br/>같이 활동해보세요!</Link></Button>
           <Button onClick={handleOnShowQRModal} css={cssBtnStyle1}><Link to={PATH.Register_HR}>바로도움요청<br/>급하게 도움이 필요할 때<br/>도움을 요청해보세요!</Link></Button>
         {/* </div> */}
       </div>
